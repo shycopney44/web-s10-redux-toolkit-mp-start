c97e8bf513e56d33dd46cef75f9be4028275c4b0
Shy Copney <shydashacopney@gmail.com>
2025-01-14 01:18:43 -0500

    complete project

--- frontend/state/quotesSlice.js
+++ frontend/state/quotesSlice.js
@@ -1,7 +1,7 @@
-// ✨ create your `quotesSlice` in this module
+import { createSlice } from "@reduxjs/toolkit";
 
-let id = 1
-const getNextId = () => id++
+let id = 1;
+export const getNextId = () => id++;
 const initialState = {
   displayAllQuotes: true,
   highlightedQuote: null,
@@ -10,7 +10,7 @@ const initialState = {
       id: getNextId(),
       quoteText: "Don't cry because it's over, smile because it happened.",
       authorName: "Dr. Seuss",
-      apocryphal: false,
+      apocryphal: true,
     },
     {
       id: getNextId(),
@@ -25,4 +25,53 @@ const initialState = {
       apocryphal: false,
     },
   ],
-}
+};
+
+export const quoteSlice = createSlice({
+  name: 'quotes',
+  initialState,
+  reducers: { // Corrected here
+    toggleVisibility(state) {
+      state.displayAllQuotes = !state.displayAllQuotes;
+    },
+    deleteQuote(state, action) {
+      state.quotes = state.quotes.filter(qt => qt.id !== action.payload);
+    },
+    editQuoteAuthenticity(state, action) {
+      const quoteToEdit = state.quotes.find(qt => qt.id === action.payload);
+      quoteToEdit.apocryphal = !quoteToEdit.apocryphal;
+    },
+    setHighlightedQuote(state, action) {
+      if (state.highlightedQuote === action.payload) {
+        state.highlightedQuote = null;
+      } else {
+        state.highlightedQuote = action.payload;
+      }
+    },
+    createQuote: {
+      prepare({ authorName, quoteText }) {
+        return {
+          payload: {
+            id: getNextId(),
+            authorName,
+            quoteText,
+            apocryphal: false,
+          },
+        };
+      },
+      reducer(state, action) {
+        state.quotes.push(action.payload);
+      },
+    }
+  },
+});
+
+export const {
+  toggleVisibility,
+  deleteQuote,
+  editQuoteAuthenticity,
+  setHighlightedQuote,
+  createQuote,
+} = quoteSlice.actions;
+
+export default quoteSlice.reducer;
